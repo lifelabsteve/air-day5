ffd2d7e19fb15805dc27fd4cff6810e491806005
lifelabsteve <lifelab@gmail.com>
2025-04-21 23:01:40 +0900

    assign6 ORM

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from .models import Tweet, Like
-# Register your models here.
+from .models import Tweet
 
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'payload', 'like_count', 'created_at')
 
-@admin.register(Like)
-class LikeAdmin(admin.ModelAdmin):
-    pass
+    def like_count(self, obj):
+        return obj.likes.count()
 
+    like_count.short_description = 'Likes'
--- tweets/models.py
+++ tweets/models.py
@@ -8,7 +8,7 @@ class Tweet(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 class Like(models.Model):
-    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE, related_name='likes')
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
