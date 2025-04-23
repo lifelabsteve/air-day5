8aab91398a40497e47d8077b1ee54e1485aff6d9
lifelabsteve <lifelab@gmail.com>
2025-04-23 23:27:03 +0900

    250423 Urls, Views and Templates

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Tweet
 
-# Create your views here.
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')  # 최신 트윗이 먼저 보이도록 정렬
+    return render(request, 'tweets/tweet_list.html', {'tweets': tweets})
