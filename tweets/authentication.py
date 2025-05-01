9438ac5f55820e28e87216113110d2183bb5e332
lifelabsteve <lifelab@gmail.com>
2025-05-01 23:10:35 +0900

    Authentication

---
+++ tweets/authentication.py
@@ -0,0 +1,16 @@
+from rest_framework.authentication import BaseAuthentication
+from django.contrib.auth.models import User
+from rest_framework.exceptions import AuthenticationFailed
+
+class UsernameAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        username = request.headers.get('X-USERNAME')
+        
+        if not username:
+            return None
+            
+        try:
+            user = User.objects.get(username=username)
+            return (user, None)
+        except User.DoesNotExist:
+            raise AuthenticationFailed('해당 사용자를 찾을 수 없습니다.') 
